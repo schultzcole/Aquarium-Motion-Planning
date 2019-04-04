73157762114f8a80129d1c19dd20afee2e16ef2e
Cole Schultz <schultzcole@gmail.com>
2019-04-04 00:38:16 -0500

    Add ability for agents to skip to the latest visible node in the path

--- Motion-Planning/Assets/Code/Agent.cs
+++ Motion-Planning/Assets/Code/Agent.cs
@@ -12,9 +12,12 @@ namespace Code
 		private bool _pathValid;
 		private int _nextNode;
 
+		private float _radius;
+
 		private void Start()
 		{
 			rb = GetComponent<Rigidbody>();
+			_radius = transform.localScale.x / 2;
 		}
 
 		public void Init(Vector2[] path)
@@ -30,22 +33,35 @@ namespace Code
 		{
 			if (!_pathValid) return;
 
+			for (int nextSeenNode = _nextNode; nextSeenNode < _path.Length; nextSeenNode++)
+			{
+				if (Physics.CheckCapsule(transform.position, _path[nextSeenNode], _radius,
+					LayerMask.GetMask("Obstacles")))
+				{
+					break;
+				}
+
+				_nextNode = nextSeenNode;
+			}
+
 			Vector2 toNext = _path[_nextNode] - (Vector2)transform.position;
 			float distToNext = toNext.magnitude;
 
-			if (distToNext < _speed * Time.deltaTime)
+			if (_nextNode == _path.Length - 1)
 			{
-				transform.position = _path[_nextNode];
-				
-				if (++_nextNode > _path.Length - 1)
+				if (distToNext < _speed * Time.deltaTime)
 				{
 					_pathValid = false;
 					rb.velocity = Vector3.zero;
 					return;
 				}
 			}
+			else if (distToNext < _radius)
+			{
+				++_nextNode;
+			}
 			
-			rb.velocity = toNext.normalized * _speed;
+			rb.velocity = Vector3.RotateTowards(rb.velocity, toNext.normalized * _speed, _speed * Time.deltaTime / _radius, _speed);
 		}
 	}
 }
