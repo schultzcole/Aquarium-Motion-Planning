5de76a0e3d14395408107278a726d0b54efec5c7
Cole Schultz <schultzcole@gmail.com>
2019-04-09 19:51:52 -0500

    Code cleanup in Agent

--- Motion-Planning/Assets/Code/Agent.cs
+++ Motion-Planning/Assets/Code/Agent.cs
@@ -2,179 +2,175 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Serialization;
-using Object = System.Object;
 using Random = UnityEngine.Random;
 
-namespace Code
+public class Agent : MonoBehaviour
 {
-	public class Agent : MonoBehaviour
+	private static List<Agent> _agents = new List<Agent>();
+	private int _agentId;
+	private float _randomOffsetX;
+	private float _randomOffsetY;
+	private float _randomOffsetZ;
+	
+	private static Collider[] _obstacles;
+	
+	[SerializeField] private float maxSpeed;
+	[SerializeField] private float angularAcceleration;
+	[SerializeField] private float maxAcceleration;
+	[SerializeField] private float velocityDamping;
+
+	[Header("Boids Config")]
+	// Distances are measured in boid radii
+	// All boids configuration variables are set in the Unity editor for quick iteration.
+	[SerializeField] private float separationDist = 1;
+	[SerializeField] private float alignmentDist = 1;
+	[SerializeField] private float cohesionDist = 1;
+	[SerializeField] private float obstacleDist = 1;
+	[SerializeField] private float separationStrength = 1;
+	[SerializeField] private float alignmentStrength = 1;
+	[SerializeField] private float cohesionStrength = 1;
+	[SerializeField] private float obstacleAvoidStrength = 1;
+	[SerializeField] private float obstacleSlideStrength = 1;
+	[SerializeField] private float centeringStrength = 1;
+	[SerializeField] private float oscillationStrength = 1;
+	[SerializeField] private float oscillationTimeScale = 1;
+	[SerializeField] private float randomStrength = 1;
+	[SerializeField] private float randomTimeScale = 1;
+	
+	private Rigidbody _rb;
+
+	private PRMGenerator _prmGen;
+
+	private float _radius;
+
+	private void Start()
 	{
-		private static List<Agent> _agents = new List<Agent>();
-		private int _agentId;
-		private float _randomOffsetX;
-		private float _randomOffsetY;
-		private float _randomOffsetZ;
-		
-		private static Collider[] _obstacles;
-		
-		[SerializeField] private float maxSpeed;
-		[SerializeField] private float angularAcceleration;
-		[SerializeField] private float maxAcceleration;
-		[SerializeField] private float velocityDamping;
-
-		[Header("Boids Config")]
-		// Distances are measured in boid radii
-		[SerializeField] private float separationDist = 1;
-		[SerializeField] private float alignmentDist = 1;
-		[SerializeField] private float cohesionDist = 1;
-		[SerializeField] private float obstacleDist;
-		[SerializeField] private float separationStrength = 1;
-		[SerializeField] private float alignmentStrength = 1;
-		[SerializeField] private float cohesionStrength = 1;
-		[SerializeField] private float obstacleAvoidStrength = 1;
-		[SerializeField] private float obstacleSlideStrength = 1;
-		[SerializeField] private float centeringStrength = 1;
-		[SerializeField] private float oscillationStrength = 1;
-		[SerializeField] private float oscillationTimeScale = 1;
-		[SerializeField] private float randomStrength = 1;
-		[SerializeField] private float randomTimeScale = 1;
-		
-		private Rigidbody _rb;
-
-		private PRMGenerator _prmGen;
-
-		private float _radius;
-
-		private void Start()
+		if (_obstacles == null)
 		{
-			if (_obstacles == null)
-			{
-				_obstacles = GameObject.FindGameObjectsWithTag("Obstacles").Select(x => x.GetComponent<Collider>())
-					.ToArray();
-			}
-			
-			_agents.Add(this);
-			_agentId = _agents.Count;
-			
-			_randomOffsetX = Random.Range(1.0f, 50.0f);
-			_randomOffsetY = Random.Range(1.0f, 50.0f);
-			_randomOffsetZ = Random.Range(1.0f, 50.0f);
-			
-			_rb = GetComponent<Rigidbody>();
-			_radius = transform.localScale.x / 2;
+			_obstacles = GameObject.FindGameObjectsWithTag("Obstacles").Select(x => x.GetComponent<Collider>())
+				.ToArray();
+		}
+		
+		_agents.Add(this);
+		_agentId = _agents.Count;
+		
+		_randomOffsetX = Random.Range(1.0f, 50.0f);
+		_randomOffsetY = Random.Range(1.0f, 50.0f);
+		_randomOffsetZ = Random.Range(1.0f, 50.0f);
+		
+		_rb = GetComponent<Rigidbody>();
+		_radius = transform.localScale.x / 2;
+	}
+
+	public void Init(PRMGenerator prmGen)
+	{
+		_prmGen = prmGen;
+	}
+
+	private void Update()
+	{
+		if (Math.Abs(Time.timeScale) < .01) return;
+		
+		var dir = _prmGen.QueryGradientField(transform.position);
+		
+		Vector3 target = _rb.velocity * (1-velocityDamping);
+		if (dir.HasValue)
+		{
+			 target = Vector3.RotateTowards(_rb.velocity, dir.Value.normalized * maxSpeed, angularAcceleration,
+				maxAcceleration);
 		}
 
-		public void Init(PRMGenerator prmGen)
+		Vector3 separation = Vector3.zero;
+		Vector3 alignment = Vector3.zero;
+		int alignNeighbors = 0;
+		Vector3 cohesion = Vector3.zero;
+		int cohesionNeighbors = 0;
+		Vector3 obstacleAvoid = Vector3.zero;
+		Vector3 obstacleSlide = Vector3.zero;
+		Vector3 oscillation = transform.right * Mathf.Sin(Time.time * oscillationTimeScale + _randomOffsetX);
+
+		// The intent of using GetHashCode on the agent ID is to make it so that agents with similar IDs do not
+		// necessarily have similar random vectors.
+		Vector3 random = Vector3.ClampMagnitude(new Vector3(
+				Mathf.PerlinNoise(Time.time * randomTimeScale + _randomOffsetX, _agentId.GetHashCode()) * 2 - 1,
+				Mathf.PerlinNoise(Time.time * randomTimeScale + _randomOffsetY, (_agentId + 7).GetHashCode()) * 2 - 1,
+				Mathf.PerlinNoise(Time.time * randomTimeScale + _randomOffsetZ, (_agentId - 11).GetHashCode()) * 2 - 1
+			) * maxSpeed, maxSpeed);
+
+		var myPos = transform.position;
+		
+		foreach (var other in _agents)
 		{
-			_prmGen = prmGen;
+			if (other == this) continue;
+
+			var toOther = other.transform.position - myPos;
+			var dist = toOther.magnitude;
+
+			if (dist < separationDist * _radius && dist > 0)
+			{
+				separation += toOther * -1 / dist;
+			}
+
+			if (dist < alignmentDist * _radius)
+			{
+				alignment += other._rb.velocity;
+				alignNeighbors++;
+			}
+
+			if (dist < cohesionDist * _radius)
+			{
+				cohesion += other.transform.position;
+				cohesionNeighbors++;
+			}
 		}
 
-		private void Update()
+		if (alignNeighbors > 0)
 		{
-			if (Math.Abs(Time.timeScale) < .01) return;
-			
-			var dir = _prmGen.QueryGradientField(transform.position);
-			
-			Vector3 target = _rb.velocity * (1-velocityDamping);
-			if (dir.HasValue)
+			alignment /= alignNeighbors;
+		}
+		
+		if (cohesionNeighbors > 0)
+		{
+			cohesion /= cohesionNeighbors;
+		}
+
+		if (_obstacles != null)
+		{
+			foreach (var obs in _obstacles)
 			{
-				 target = Vector3.RotateTowards(_rb.velocity, dir.Value.normalized * maxSpeed, angularAcceleration,
-					maxAcceleration);
-			}
+				var obsTransform = obs.transform;
+				var closestPoint = Physics.ClosestPoint(myPos, obs, obsTransform.position,
+					obsTransform.rotation);
 
-			Vector3 separation = Vector3.zero;
-			Vector3 alignment = Vector3.zero;
-			int alignNeighbors = 0;
-			Vector3 cohesion = Vector3.zero;
-			int cohesionNeighbors = 0;
-			Vector3 obstacleAvoid = Vector3.zero;
-			Vector3 obstacleSlide = Vector3.zero;
-			Vector3 oscillation = transform.right * Mathf.Sin(Time.time * oscillationTimeScale + _randomOffsetX);
-
-			// The intent of using GetHashCode on the agent ID is to make it so that agents with similar IDs do not
-			// necessarily have similar random vectors.
-			Vector3 random = Vector3.ClampMagnitude(new Vector3(
-					Mathf.PerlinNoise(Time.time * randomTimeScale + _randomOffsetX, _agentId.GetHashCode()) * 2 - 1,
-					Mathf.PerlinNoise(Time.time * randomTimeScale + _randomOffsetY, (_agentId + 7).GetHashCode()) * 2 - 1,
-					Mathf.PerlinNoise(Time.time * randomTimeScale + _randomOffsetZ, (_agentId - 11).GetHashCode()) * 2 - 1
-				) * maxSpeed, maxSpeed);
-
-			var myPos = transform.position;
-			
-			foreach (var other in _agents)
-			{
-				if (other == this) continue;
-
-				var toOther = other.transform.position - myPos;
-				var dist = toOther.magnitude;
-
-				if (dist < separationDist * _radius && dist > 0)
+				var toPoint = closestPoint - myPos;
+				var toPointDist = toPoint.magnitude;
+				if (toPointDist < obstacleDist * _radius)
 				{
-					separation += toOther * -1 / dist;
-				}
-
-				if (dist < alignmentDist * _radius)
-				{
-					alignment += other._rb.velocity;
-					alignNeighbors++;
-				}
-
-				if (dist < cohesionDist * _radius)
-				{
-					cohesion += other.transform.position;
-					cohesionNeighbors++;
-				}
-			}
-
-			if (alignNeighbors > 0)
-			{
-				alignment /= alignNeighbors;
-			}
-			
-			if (cohesionNeighbors > 0)
-			{
-				cohesion /= cohesionNeighbors;
-			}
-
-			if (_obstacles != null)
-			{
-				foreach (var obs in _obstacles)
-				{
-					var obsTransform = obs.transform;
-					var closestPoint = Physics.ClosestPoint(myPos, obs, obsTransform.position,
-						obsTransform.rotation);
-
-					var toPoint = closestPoint - myPos;
-					var toPointDist = toPoint.magnitude;
-					if (toPointDist < obstacleDist * _radius)
+					if (toPointDist > 0)
 					{
-						if (toPointDist > 0)
-						{
-							obstacleAvoid += toPoint * -1 / toPointDist;
-						}
-						obstacleSlide += (target - toPoint * Vector3.Dot(target, toPoint.normalized)) * obstacleSlideStrength;
+						obstacleAvoid += toPoint * -1 / toPointDist;
 					}
+					obstacleSlide += (target - toPoint * Vector3.Dot(target, toPoint.normalized)) * obstacleSlideStrength;
 				}
 			}
+		}
 
-			var toCenter = new Vector3(0, 10, 0) - myPos;
+		var toCenter = new Vector3(0, 10, 0) - myPos;
 
-			var newVel = target +
-			             separation * separationStrength +
-			             alignment * alignmentStrength +
-			             cohesion * cohesionStrength +
-			             obstacleAvoid * obstacleAvoidStrength +
-			             obstacleSlide +
-			             toCenter * centeringStrength +
-			             oscillation * oscillationStrength +
-			             random * randomStrength;
+		var newVel = target +
+		             separation * separationStrength +
+		             alignment * alignmentStrength +
+		             cohesion * cohesionStrength +
+		             obstacleAvoid * obstacleAvoidStrength +
+		             obstacleSlide +
+		             toCenter * centeringStrength +
+		             oscillation * oscillationStrength +
+		             random * randomStrength;
 
 //			newVel = random;
-			
-			_rb.velocity = Vector3.ClampMagnitude(newVel, maxSpeed);
-			
-			transform.LookAt(myPos + _rb.velocity.normalized, Vector3.up);
-		}
+		
+		_rb.velocity = Vector3.ClampMagnitude(newVel, maxSpeed);
+		
+		transform.LookAt(myPos + _rb.velocity.normalized, Vector3.up);
 	}
 }
